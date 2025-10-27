2baaf4b8232cfa5259c90de0f4167973d9589e2f
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2025-10-27 14:40:22 +0000

    JS

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require("path")
 
 app.get('/', (req,res) => {
-    res.status(200).sendFile(path.dir('/public/main.html'));
+    res.status(200).sendFile(path.join(__dirname,'/public/main.html'));
 });
 
 app.listen(5000, () => {
